d40e94bec2a68aca452bb65d26175ff7b69627c0
kimyeonghwi <pwrkim12@gmail.com>
2021-08-08 17:24:49 +0900

    로그인페이지 완료

--- js/main.js
+++ js/main.js
@@ -1,22 +1,5 @@
 
 
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-
-searchEl.addEventListener('click', function(){
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus' , function(){
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur' , function(){
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
 
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -157,5 +140,3 @@ spyEls.forEach(function (spyEl) {
 }); 
 
 //텍스트 
-const thisyear = document.querySelector('.this-year');
-thisyear.textContent = new Date().getFullYear();
